f58404d6ca7775fdab56af519dab4627f0c76c4d
菅野　裕介 <kanno@fpc76.fundelyone.local>
2020-05-25 15:47:08 +0900

    Add:adminフラグ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_060601) do
+ActiveRecord::Schema.define(version: 2020_05_25_064414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_060601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "delete_flag", default: 0, null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
