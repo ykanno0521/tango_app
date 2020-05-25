64be8d730fce45ae61fa8b9e5715d35a54b30054
菅野　裕介 <kanno@fpc76.fundelyone.local>
2020-05-25 17:57:38 +0900

    Add:ログアウト機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: session_params[:name])
 
-    if user&authenticate(session_params[:password])
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to '/top', notice: 'ログインしました'
     else
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to '/top', notice: 'ログアウトしました'
   end
 
   private
