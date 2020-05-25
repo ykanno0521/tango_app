35b47ae17a95662a7c7d25af44456f7ad1e6ea92
菅野　裕介 <kanno@fpc76.fundelyone.local>
2020-05-25 17:53:33 +0900

    Add:ログイン機能

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: session_params[:name])
+
+    if user&authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to '/top', notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:name, :password)
+  end
+end
